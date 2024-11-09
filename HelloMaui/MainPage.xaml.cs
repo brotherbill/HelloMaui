c1526d7ad02637cbe8c010c5997e14127812ff9f
William Steinberg <brotherbill@mail.com>
2024-11-08 20:48:48 -0500

    Getting Started End

--- HelloMaui/MainPage.xaml.cs
+++ HelloMaui/MainPage.xaml.cs
@@ -7,8 +7,8 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		Content = new Label()
-			.Text("This is a Label")
-			.Center()
-			.TextCenter();
+			.Text("This is a Label")  
+			.Center()			// Center Horizontally and Vertically
+			.TextCenter();		// Center text horizontally
 	}
 }
